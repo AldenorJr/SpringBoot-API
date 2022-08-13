74285d3edfff45fcc293d033916afa0dc5f3c60c
YTNexus <Aldenorjr500@yahoo.com.br>
2022-08-13 15:55:47 -0300

    Primeiro commit, Spring boot project

---
+++ src/main/java/me/nexus/spring/testeproject/TesteprojectApplication.java
@@ -0,0 +1,13 @@
+package me.nexus.spring.testeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/nexus/spring/testeproject/TesteprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package me.nexus.spring.testeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
